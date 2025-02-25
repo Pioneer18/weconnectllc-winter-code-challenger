351103087decd5589d8afe454b27eeb186b628ae
Pioneer18 <sellsj14@gmail.com>
2025-02-25 04:59:30 -0500

    1. added a selector for setting a new state, pageSize. Then I'm passing that pageSize state into the useTaskStateManager todo: set the pageSize query param with the passed in state

--- src/components/task-list/task-list.tsx
+++ src/components/task-list/task-list.tsx
@@ -3,16 +3,22 @@
 import { TaskItem } from "../task-item/task-item";
 import TaskErrorBoundary from "../error-handling/task-error-boundary";
 import LoadingState from "./task-list-loading";
-import React from "react";
+import React, { useState } from "react";
 import useTaskStateManager from "@/hooks/use-task-state-manager";
 import { TaskError } from "../error-handling/task-error-alert";
 
 export default function TaskList() {
-  const [loading, hasMore, error, tasks, nextPage] = useTaskStateManager([], 1);
+  const [pageSize, setPageSize] = useState(5);
+  const [loading, hasMore, error, tasks, nextPage] = useTaskStateManager([], 1, pageSize);
 
   const handleLoadMore = () => {
     nextPage();
   };
+
+  const handlePageSizeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newPageSize = parseInt(e.target.value);
+    setPageSize(newPageSize);
+  }
 
   return (
     <TaskErrorBoundary>
@@ -20,10 +26,13 @@ export default function TaskList() {
         {error &&
           <TaskError error={error} />
         }
+
         {tasks.map((task) => (
           <TaskItem key={task.id} task={task} />
         ))}
+
         {loading && <LoadingState />}
+        
         {!loading && hasMore &&
           <button
             onClick={handleLoadMore}
@@ -32,6 +41,19 @@ export default function TaskList() {
             Load more tasks
           </button>
         }
+
+        <div className="flex items-center space-x-2">
+          <label >Choose Number of Tasks to load</label>
+          <select 
+            className="w-1/10 p-2 border rounded"
+            value={pageSize}
+            onChange={handlePageSizeChange}
+          >
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={25}>25</option>
+          </select>
+        </div>
       </div>
     </TaskErrorBoundary>
   );
